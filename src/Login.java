d1a4650981d9c7dd43834d6be36852932afdae3a
GitHubMubashir <mubashirtest1@gmail.com>
2023-01-16 13:26:34 +0500

    changes

---
+++ src/Login.java
@@ -0,0 +1,30 @@
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Login 
+{
+	public static void main(String[] args)
+	{
+		/*System.setProperty("webdriver.chrome.driver","C:\\Users\\dev\\Downloads\\geckodriver-v0.32.0-win-aarch64");
+		WebDriver driver1= new FirefoxDriver();
+		driver1.get("https://google.com/");
+		System.out.println(driver1.getTitle());
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\dev\\Downloads\\work\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://google.com/");
+		//driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		System.out.println(driver.getTitle());
+		
+		int a=2;
+		int	b=3;
+		int Sum=a+b;
+		System.out.println("Sum is " +Sum);
+*/
+	class2	m = new class2(); 
+	System.out.println(m.ValidateHeader());
+*/	fdssdf
+		
+	}
+}
---
+++ src/class2.java
@@ -0,0 +1,10 @@
+
+public class class2 
+{
+	public int ValidateHeader() 
+	{
+	  System.out.println("Class 2");
+      return 2;
+	}
+
+}
